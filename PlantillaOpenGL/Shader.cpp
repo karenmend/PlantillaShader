9966460929202b426b7129c871207f7d34d85fbe
Karen Mendivil <karenlili14@gmail.com>
2019-02-28 08:51:04 -0700

    Llegamos al paso 8.

--- PlantillaOpenGL/Shader.cpp
+++ PlantillaOpenGL/Shader.cpp
@@ -18,7 +18,7 @@ Shader::Shader(const char * rutaVertex, const char * rutaFragment) {
 	}
 	else
 	{
-		cout << "No se pudo abrir el archivo."<< rutaVertex << endl;
+		cout << "No se pudo abrir el archivo." << rutaVertex << endl;
 	}
 
 	string codigoFragmentShader;
@@ -41,18 +41,60 @@ Shader::Shader(const char * rutaVertex, const char * rutaFragment) {
 	shaderID = glCreateProgram();
 	GLuint vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
 	GLuint fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
-	
+
 	//2.- Cargar el codigo del Shader 
 	glShaderSource(vertexShaderID, 1, &cadenaCodigoVertex, NULL);
 	glShaderSource(fragmentShaderID, 1, &cadenaCodigoFragment, NULL);
-	
+
 	//3.- Compilar los shaders
 	glCompileShader(vertexShaderID);
 	glCompileShader(fragmentShaderID);
 
+	//4.- Verificar errores de compilación
+	verificarCompilacion(vertexShaderID);
+	verificarCompilacion(fragmentShaderID);
+
+	//5.- Adjuntar shader al programa
+	glAttachShader(shaderID, vertexShaderID);
+	glAttachShader(shaderID, fragmentShaderID);
+
+	//6.- Vincular el programa 
+	glLinkProgram(shaderID);
+
+	//7.- Verificar la vinculacion
+	verificarVinculacion(shaderID);
+
+	//8.- Usar el programa
+	glUseProgram(shaderID);
 }
 
 void Shader::verificarCompilacion(GLuint id) {
 	GLint resultado = GL_FALSE;
-	int longitudLog = 0;
+	GLint longitudLog = 0;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &resultado);
+	glGetShaderiv(id, GL_INFO_LOG_LENGTH, &longitudLog);
+
+	if(longitudLog > 0) {
+		vector<char> mensajeError(longitudLog);
+		glGetShaderInfoLog(id, longitudLog, NULL, &mensajeError[0]);
+
+		for (vector<char>::const_iterator i = mensajeError.begin(); i != mensajeError.end(); i++) {
+			cout << *i;
+		}
+	}
+
+}
+
+void Shader::verificarVinculacion(GLuint id) {
+	GLint estadoVinculacion, estadoValidacion; 
+
+	glGetProgramiv(id, GL_LINK_STATUS, &estadoVinculacion);
+	if (estadoVinculacion == GL_FALSE) {
+		cout << "No se pudo vincular programa." << endl;
+
+	}
+	glGetProgramiv(id, GL_VALIDATE_STATUS, &estadoValidacion);
+	if (estadoValidacion == GL_FALSE) {
+		cout << "No se pudo validar la vinculacion." << endl;
+	}
 }
